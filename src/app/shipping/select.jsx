329cd04468271164660553a8e147c3ffec90bfb4
Githubmagali <magalivictoria85068@gmail.com>
2024-03-04 23:22:12 -0300

    change select shipping takeaway

--- src/app/shipping/select.jsx
+++ src/app/shipping/select.jsx
@@ -2,8 +2,6 @@
 import { location } from '@/assets/offices'
 
 
-
-
 function SelectSucursal(){
 
  
@@ -15,8 +13,6 @@ function SelectSucursal(){
     
        <div className="relative">
          <select
-           id="customer"
-           name="customerId"
            className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
            defaultValue=""
          >
