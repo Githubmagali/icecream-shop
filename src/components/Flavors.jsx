e4c043ae199ea0c3c6ba4538a17edc615b949537
realisticattorney <g.axl.aquila@gmail.com>
2024-03-22 20:57:41 -0300

    Move list of flavors to component

---
+++ src/components/Flavors.jsx
@@ -0,0 +1,54 @@
+function Flavors() {
+  return (
+    <div>
+      <div className="lg:grid lg:grid-cols-3  justify-center text-center gap-x-20">
+        <div className="col-span-1  sm:py-10">
+          <h1 className="text-3xl font-bold text-yellow-500">Chocolate</h1>
+          {chocolateData.map((item, index) => (
+            <p key={index}>{item.name}</p>
+          ))}
+        </div>
+        <div className="col-span-1  sm:py-10">
+          <h1 className="text-3xl font-bold text-yellow-500">Dulce de leche</h1>
+          {dulce.map((item, index) => (
+            <p key={index}>{item.name}</p>
+          ))}
+          <h1 className="text-3xl font-bold text-yellow-500 pt-10">
+            Recomends
+          </h1>
+          {recomends.map((item, index) => (
+            <p key={index}>{item.name}</p>
+          ))}
+        </div>
+        <div className="col-span-1 sm:py-10 ">
+          <h1 className="text-3xl font-bold text-yellow-500 pt-20 ">Creams</h1>
+          {creams.map((item, index) => (
+            <p key={index}>{item.name}</p>
+          ))}
+        </div>
+        <div className="col-span-1 sm:py-10 ">
+          <h1 className="text-3xl font-bold text-yellow-500">Fruits</h1>
+          {fruits.map((item, index) => (
+            <p key={index}>{item.name}</p>
+          ))}
+        </div>
+        <div className="col-span-1 sm:py-10">
+          <h1 className="text-3xl font-bold text-yellow-500">
+            Sweet Oleasures
+          </h1>
+          {sweetOleasures.map((item, index) => (
+            <p key={index}>{item.name}</p>
+          ))}
+        </div>
+        <div className="col-span-1 sm:py-10">
+          <h1 className="text-3xl font-bold text-yellow-500">Clasic</h1>
+          {clasic.map((item, index) => (
+            <p key={index}>{item.name}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Flavors;
