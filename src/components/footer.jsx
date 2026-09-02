de756bb3f0660fe764832132f4a143d859cff7fd
Magali Fernandez <magalivictoria85068@gmail.com>
2026-09-02 19:08:31 -0300

    footer listo

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,20 +2,30 @@ function Footer() {
     return (
         <div className="pt-10 left-0 right-0 text-white ">
             <div className="pt-10 left-0 right-0 text-white p-2 bg-slate-100 pb-4 lg:pb-10 ">
-                <div className=" flex items-center justify-center px-5">
+                <div className="flex items-center justify-center px-5">
                     <div className="flex-1">
-                        <p className="text-gray-400 text-xs">Kosher certificate</p>
-                        <p className="text-gray-400 text-xs">Contact consumer protection 0-800-777-7</p>
-                    </div>
-                    <div className="flex-1">
-                        <div />
-                        <p className="text-gray-400 text-xs">Privacy Policy</p>
-                        <p className="text-gray-400 text-xs">Personal data protection</p>
-                    </div>
-                    <div className="flex-1">
-                        <a href="mailto:magalivictoria85068@gmail.com" className="text-gray-400 flex items-center justify-center text-xs ">&#169;Magali Victoria Fernandez</a>
+                        <div className="flex-1">
+                            <p className="text-gray-400 text-xs">Kosher certificate</p>
+                        </div>
+                        <div className="flex-1">
+                            <p className="text-gray-400 text-xs">Contact consumer protection 0-800-777-7</p>
+                        </div>
                     </div>
 
+                    <div className="flex-1">
+                        <div className="flex-1">
+                            <p className="text-gray-400 text-xs">Privacy Policy</p>
+                        </div>
+                        <div className="flex-1">
+                            <p className="text-gray-400 text-xs">Personal data protection</p>
+                        </div>
+                    </div>
+                    <div className="flex-1 hidden lg:flex">
+                        <a href="mailto:magalivictoria85068@gmail.com" className="text-gray-400 flex items-center justify-center text-xs ">&#169;Magali Victoria Fernandez</a>
+                    </div>
+                </div>
+                 <div className="flex justify-center lg:hidden flex-1 py-4">
+                        <a href="mailto:magalivictoria85068@gmail.com" className="text-gray-400 flex items-center justify-center text-xs ">&#169;Magali Victoria Fernandez</a>
                 </div>
             </div>
         </div>
